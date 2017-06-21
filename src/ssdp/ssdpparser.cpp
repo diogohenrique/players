e0d769cd9c90e0c7fae53cc8a51ce015bc2ac043
Guilherme Almeida <guilkalmeida@gmail.com>
2017-06-21 12:43:45 -0300

    Attempt to fix crash when ReadLine is given small amount of data (fabric crash 111)

--- src/ssdp/ssdpparser.cpp
+++ src/ssdp/ssdpparser.cpp
@@ -205,6 +205,11 @@ SSDP_TYPE SSDPParser::GetType(){
 
 int SSDPParser::ReadLine(u8 *buf, u32 len, u8 **restbuf, u32 *restlen){
     int ret = 0;
+    
+    // If len is <2 it's not possible that we're going to find \r\n
+    if (len < 2) {
+        return -1;
+    }
 
     //Search the \r\n
     u8 *pos = buf;
@@ -216,8 +221,12 @@ int SSDPParser::ReadLine(u8 *buf, u32 len, u8 **restbuf, u32 *restlen){
         pos++;
         pos++;
         *restlen = (unsigned int)((buf+len)-pos);
-        *restbuf = pos;
-        ret = (unsigned int)((pos-buf)-2);
+        if( *restlen == 0 ){
+            ret = -1;//eof
+        }else{
+            *restbuf = pos;
+            ret = (unsigned int)((pos-buf)-2);
+        }
     }else{
         ret = -1;//eof
     }
